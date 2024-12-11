bb9276cbe6973b977d3fc837b0decefcc5a96d63
torat3158 <torat3158@gmail.com>
2024-12-11 09:42:41 +0900

    クロスオリジンの設定にalbのドメインを追加

--- backend/main.go
+++ backend/main.go
@@ -1,37 +1,40 @@
 package main
 
 import (
-    "github.com/gin-contrib/cors"
-    "github.com/gin-gonic/gin"
-    "todo/handlers"
-    "todo/config"
+	"todo/config"
+	"todo/handlers"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    r := gin.Default()
+	r := gin.Default()
 
-    // CORS設定
-    r.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"http://localhost:3000"},
-        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
-        AllowHeaders:     []string{"Origin", "Content-Type"},
-        ExposeHeaders:    []string{"Content-Length"},
-        AllowCredentials: true,
-    }))
+	// CORS設定
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:3000",
+			"http://todo-elb-121795785.ap-northeast-1.elb.amazonaws.com",
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
-    config.ConnectDB() // データベース接続
+	config.ConnectDB() // データベース接続
 
-    // ルーティング設定
-    r.GET("/api/tasks", handlers.GetTasks)
-    r.POST("/api/tasks", handlers.CreateTask)
-    r.PUT("/api/tasks/:id", handlers.UpdateTask)
-    r.DELETE("/api/tasks/:id", handlers.DeleteTask)
-    r.GET("/health", func(c *gin.Context) {
-   	 c.JSON(200, gin.H{
-        	"status": "healthy",
-   	 })
-    })
+	// ルーティング設定
+	r.GET("/api/tasks", handlers.GetTasks)
+	r.POST("/api/tasks", handlers.CreateTask)
+	r.PUT("/api/tasks/:id", handlers.UpdateTask)
+	r.DELETE("/api/tasks/:id", handlers.DeleteTask)
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "healthy",
+		})
+	})
 
-    r.Run(":8080")
+	r.Run(":8080")
 }
-
