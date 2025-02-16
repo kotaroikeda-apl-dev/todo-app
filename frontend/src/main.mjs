24a090d1f951a00dc8a9fb90ec2ab9abfb8e0abc
torat3158 <torat3158@gmail.com>
2025-02-16 17:27:55 +0900

    戻す機能追加

--- frontend/src/main.mjs
+++ frontend/src/main.mjs
@@ -4,6 +4,10 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteTodo(inputText);
+}
+
+const createIncompleteTodo = (todo) => {
   const li = document.createElement("li");
 
   const div = document.createElement("div");
@@ -11,7 +15,7 @@ const onClickAdd = () => {
 
   const p = document.createElement("p");
   p.className = "todo-item";
-  p.innerText = inputText;
+  p.innerText = todo;
 
   const completeButton = document.createElement("button");
   completeButton.innerText="完了";
@@ -21,6 +25,11 @@ const onClickAdd = () => {
     completeButton.remove();
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      const todoText = backButton.previousElementSibling.innerText;
+      createIncompleteTodo(todoText);
+      backButton.closest("li").remove();
+    });
     moveTarget.firstElementChild.appendChild(backButton);
     document.getElementById("complete-list").appendChild(moveTarget);
   });
@@ -38,6 +47,6 @@ const onClickAdd = () => {
   li.appendChild(div);
 
   document.getElementById("incomplete-list").appendChild(li);
-}
+};
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
