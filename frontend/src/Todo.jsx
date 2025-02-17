7a3c140923c7731a0945b5c253dece309085c834
torat3158 <torat3158@gmail.com>
2025-02-17 23:52:44 +0900

    TODOの上限設定

--- frontend/src/Todo.jsx
+++ frontend/src/Todo.jsx
@@ -42,9 +42,14 @@ export const Todo = () => {
     setIncompleteTodos(newIncompleteTodos);
   };
 
+  const isMaxLimitIncompleteTodos = incompleteTodos.length >= 5;
+
   return (
     <>
-      <InputTodo todoText={todoText} onChange={onChangeTodoText} onClick={onClickAdd}/>
+      <InputTodo todoText={todoText} onChange={onChangeTodoText} onClick={onClickAdd} disabled={isMaxLimitIncompleteTodos}/>
+      {isMaxLimitIncompleteTodos && (
+        <p style={{color:"red"}}>登録できるTODOは5個までだよ〜。消化しろ〜。</p>
+      )}
       <IncompleteTodos todos={incompleteTodos} onClickComplete={onClickComplete} onClickDelete={onClickDelete}/>
       <CompleteTodos todos={completeTodos} onClick={onClickback}/>
     </>
